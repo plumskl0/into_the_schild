ca630554a02b75149075573b60acfe0601896a47
plumskl0 <plumskl0@users.noreply.github.com>
2018-12-02 12:20:01 +0100

    Stabile Version des Requesters, die über das Terminal läuft.

--- requester/requester.py
+++ requester/requester.py
@@ -8,7 +8,7 @@
         - Inputordner für Dateien
         - hier werden Dateien abgelegt, die an das NN gesendet werden sollen
         - Unterordner:
-            - /done         - Klassifizierte Bilder 
+            - /done         - Klassifizierte Bilder
             - /done/error   - Bilder die durch einen Fehler nicht klassifiziert wurden
             - /trash        - Dateien die keine PNGs sind
     req_out
@@ -27,8 +27,11 @@
 
 import os
 import time
+import json
 import logging
+import imageio
 import requests
+import numpy as np
 import configparser as cfgp
 from lxml import etree
 from datetime import datetime
@@ -51,6 +54,9 @@ PARAM_REQ = 'Requester'
 PARAM_URL = 'url'
 PARAM_KEY = 'key'
 PARAM_DEF_KEY_VAL = 'Api-Key einfuegen'
+PARAM_DELAY = 'delay'
+# Alle 10 Sekunden auf neue Dateien prüfen
+PARAM_DEF_DELAY_VAL = 10
 
 # XML Elemente und Attribute
 XML_ROOT = 'request_history'
@@ -60,7 +66,7 @@ XML_RESULT = 'result'
 
 XML_ATR_DATE = 'date'
 XML_ATR_DTYPE = 'dtype'
-XML_ATR_DATE_FORMAT = '%H:%M:%S %d-%m-%Y'
+XML_ATR_DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
 
 # Prüfen ob Dateien und Ordner vorhanden sind, ggfs. anlegen
 
@@ -68,7 +74,7 @@ XML_ATR_DATE_FORMAT = '%H:%M:%S %d-%m-%Y'
 def initLogging(logFile='requester.log'):
     logging.basicConfig(
         # filename=logFile,
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s:%(levelname)s:%(message)s'
     )
 
@@ -86,7 +92,18 @@ def getMyLogger():
 
 def checkFilesFolders():
     createDir(dirIn, dirTrash, dirDone, dirError, dirOut)
-    createFile(fileConfig, fileHistory)
+
+    log = getMyLogger()
+    if not os.path.exists(fileConfig):
+        log.debug('Creating File: {}'.format(fileConfig))
+        with open(fileConfig, 'w') as cfgFile:
+            config = createConfigTemplate()
+            config.write(cfgFile)
+
+    if not os.path.exists(fileHistory):
+        log.debug('Creating File: {}'.format(fileHistory))
+        root = createHistoryTemplate()
+        xmlToFile(root)
 
 
 def createDir(*args):
@@ -97,21 +114,12 @@ def createDir(*args):
             os.makedirs(d)
 
 
-def createFile(*args):
-    log = getMyLogger()
-    for f in args:
-        if not os.path.exists(f):
-            log.debug('Creating File: {}'.format(f))
-            with open(f, 'w') as myFile:
-                config = createConfigTemplate()
-                config.write(myFile)
-
-
 def createConfigTemplate():
     getMyLogger().debug('Creating Config File: {}'.format(PARAM_REQ))
     config = cfgp.ConfigParser()
     config[PARAM_REQ] = {PARAM_URL: 'http://www.example.com',
-                         PARAM_KEY: PARAM_DEF_KEY_VAL}
+                         PARAM_KEY: PARAM_DEF_KEY_VAL,
+                         PARAM_DELAY: PARAM_DEF_DELAY_VAL}
     return config
 
 
@@ -122,11 +130,12 @@ def getConfig():
         return config
 
 
-def getUrlKey():
+def getConfigParams():
     config = getConfig()
 
     url = None
     key = None
+    delay = None
 
     if config.has_option(PARAM_REQ, PARAM_URL):
         url = config.get(PARAM_REQ, PARAM_URL)
@@ -134,7 +143,10 @@ def getUrlKey():
     if config.has_option(PARAM_REQ, PARAM_KEY):
         key = config.get(PARAM_REQ, PARAM_KEY)
 
-    return url, key
+    if config.has_option(PARAM_REQ, PARAM_DELAY):
+        delay = int(config.get(PARAM_REQ, PARAM_DELAY))
+
+    return url, key, delay
 
 
 def sendRequest(img):
@@ -152,12 +164,44 @@ def createHistoryTemplate():
 
     # Elemente erzeugen
     request = etree.SubElement(root, XML_REQUEST)
+    result = etree.SubElement(request, XML_RESULT)
     image = etree.SubElement(request, XML_IMAGE)
-    result = etree.Subelement(request, XML_RESULT)
 
     # Beispiel Attribute
     request.attrib[XML_ATR_DATE] = XML_ATR_DATE_FORMAT
+    request.attrib['info_text'] = 'Beispieleintrag'
     image.attrib[XML_ATR_DTYPE] = 'np.int32'
+    result.text = 'numpy.array([x,y])'
+    return root
+
+
+def addToHistory(classArr, imageArr, now):
+    root = readHistory()
+
+    # Neuen Request erzeugen
+    request = etree.SubElement(root, XML_REQUEST)
+    result = etree.SubElement(request, XML_RESULT)
+    image = etree.SubElement(request, XML_IMAGE)
+
+    # Attribute und Texte hinzufügen
+    request.attrib[XML_ATR_DATE] = now.strftime(XML_ATR_DATE_FORMAT)
+    image.attrib[XML_ATR_DTYPE] = imageArr.dtype.name
+    result.text = json.dumps(classArr)
+    image.text = json.dumps(imageArr.tolist())
+
+    xmlToFile(root)
+
+
+def xmlToFile(root):
+    xmlString = etree.tostring(root, encoding='UTF-8', pretty_print=True)
+    with open(fileHistory, 'w') as hisFile:
+        hisFile.write(xmlString.decode('UTF-8'))
+
+
+def readHistory():
+    root = None
+    with open(fileHistory, 'r') as hisFile:
+        root = etree.fromstring(hisFile.read())
     return root
 
 
@@ -168,7 +212,7 @@ log.debug('Folder check done')
 # Config Einträge prüfen
 # Ohne Api-Key nichts machen
 run = 0
-url, key = getUrlKey()
+url, key, delay = getConfigParams()
 
 # Nicht default Value und nicht leer
 if not PARAM_DEF_KEY_VAL == key or not key:
@@ -176,52 +220,62 @@ if not PARAM_DEF_KEY_VAL == key or not key:
     log.info('API-Key found')
 else:
     log.error('No API-Key defined in requester.ini')
-# while run:
+while run:
 
     # while True: -- Hier kommt später die Schleife hin
     # Input Ordner auf Bilder überprüfen
-_, _, files = next(os.walk(dirIn))
+    _, _, files = next(os.walk(dirIn))
 
-log.info('Files to process {}'.format(len(files)))
+    log.info('Files to process {}'.format(len(files)))
 
-# for f in files:
-f = files[0]
-# Dateiname/pfad erzeugen
-imgPath = os.path.join(dirIn, f)
+    for f in files:
+        # Dateiname/pfad erzeugen
+        imgPath = os.path.join(dirIn, f)
 
-# Nur PNG Bilder einlesen
-if 'png' in imgPath.lower():
-    log.info('Opening file {}'.format(imgPath))
-    img = open(imgPath, 'rb')
+        # Nur PNG Bilder einlesen
+        if 'png' in imgPath.lower():
+            log.info('Opening file {}'.format(imgPath))
+            img = open(imgPath, 'rb')
 
-    myData = {'key': key}
-    myFiles = {'image': img}
+            myData = {'key': key}
+            myFiles = {'image': img}
 
-    response = requests.post(url, data=myData, files=myFiles)
-    img.close()
+            response = requests.post(url, data=myData, files=myFiles)
+            img.close()
 
-    dest = ''
-    now = datetime.now()
-    # Aktuelle Zeit für den Dateinamen nutzen
-    newName = '{}.png'.format(now.strftime('%Y-%m-%d_%H_%M_%S'))
+            dest = ''
+            now = datetime.now()
+            # Aktuelle Zeit für den Dateinamen nutzen
+            newName = '{}.png'.format(now.strftime('%Y-%m-%d_%H_%M_%S'))
 
-    if response.ok:
-        log.info('Image sucessfully transfered')
+            if response.ok:
+                log.info('Image sucessfully transfered')
 
-        # Response verarbeiten
-        classification = response.json()
+                # Response verarbeiten
+                classArr = response.json()
+                # Bild als numpy Array einlesen
+                imageArr = imageio.imread(imgPath)
 
-        # In done Ordner verschieben
-        dest = os.path.join(dirDone, newName)
-    else:
-        # In done/error Ordner verschieben
-        log.error('Request error: {}'.format(response.text))
-        dest = os.path.join(dirError, newName)
+                # Anfrage zur Historie hinzufügen
+                addToHistory(classArr, imageArr, now)
 
-    log.info('Moving file to: {}'.format(dest))
-    os.rename(imgPath, dest)
-else:
-    log.info('File "{}" is not an PNG moving to trash'.format(f))
+                # Dateinamen für done Ordner
+                dest = os.path.join(dirDone, newName)
+            else:
+                # Dateinamen done/error Ordner
+                log.error('Request error: {}'.format(response.text))
+                dest = os.path.join(dirError, newName)
 
-    trash = os.path.join(dirTrash, f)
-    os.rename(imgPath, trash)
+            log.info('Moving file to: {}'.format(dest))
+            os.rename(imgPath, dest)
+        else:
+            log.info('File "{}" is not an PNG moving to trash'.format(f))
+
+            trash = os.path.join(dirTrash, f)
+            os.rename(imgPath, trash)
+
+        # Eine Sekunde warten damit die Requests nicht zu schnell bearbeitet werden.
+        # Evtl. unnötig, da die verarbeitung bereits länger als eine Sekunde dauert
+        time.sleep(1)
+
+    time.sleep(delay)
