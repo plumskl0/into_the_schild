87d399f79c2693138fb996215f16b12365a348f4
plumskl0 <plumskl0@users.noreply.github.com>
2018-12-11 15:26:53 +0100

    Requester in eine Klasse umgebaut und neue Funktionalität hinzugefügt.

--- requester/requester.py
+++ requester/requester.py
@@ -37,7 +37,7 @@ from lxml import etree
 from datetime import datetime
 
 # Ordner
-root = './'
+root = './its_request'
 dirIn = os.path.join(root, 'req_in')
 dirDone = os.path.join(dirIn, 'done')
 dirTrash = os.path.join(dirIn, 'trash')
@@ -46,7 +46,7 @@ dirOut = os.path.join(root, 'req_out')
 
 # Dateien
 fileConfig = 'requester.ini'
-fileHistory = 'req_history.xml'
+fileHistory = os.path.join(root, 'req_history.xml')
 fileLog = 'requester.log'
 
 # Default Config Werte
@@ -55,8 +55,10 @@ PARAM_URL = 'url'
 PARAM_KEY = 'key'
 PARAM_DEF_KEY_VAL = 'Api-Key einfuegen'
 PARAM_DELAY = 'delay'
+PARAM_XML = 'xml'
 # Alle 10 Sekunden auf neue Dateien prüfen
 PARAM_DEF_DELAY_VAL = 10
+PARAM_DEF_XML_VAL = False
 
 # XML Elemente und Attribute
 XML_ROOT = 'request_history'
@@ -74,15 +76,15 @@ XML_ATR_DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
 def initLogging(logFile='requester.log'):
     logging.basicConfig(
         # filename=logFile,
-        level=logging.INFO,
+        level=logging.DEBUG,
         format='%(asctime)s:%(levelname)s:%(message)s'
     )
 
     log = logging.getLogger('requester')
 
-    # ch = logging.StreamHandler()
-    # ch.setLevel(logging.DEBUG)
-    # log.addHandler(ch)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    log.addHandler(ch)
     return log
 
 
@@ -90,192 +92,204 @@ def getMyLogger():
     return logging.getLogger('requester')
 
 
-def checkFilesFolders():
-    createDir(dirIn, dirTrash, dirDone, dirError, dirOut)
+class Requester:
 
-    log = getMyLogger()
-    if not os.path.exists(fileConfig):
-        log.debug('Creating File: {}'.format(fileConfig))
-        with open(fileConfig, 'w') as cfgFile:
-            config = createConfigTemplate()
-            config.write(cfgFile)
+    def __init__(self):
+        self.logger = getMyLogger()
 
-    if not os.path.exists(fileHistory):
-        log.debug('Creating File: {}'.format(fileHistory))
-        root = createHistoryTemplate()
-        xmlToFile(root)
+        # Um beim ersten Start eine Beispiel XML zu erzeugen
+        self.xmlHistory = True
+        self.checkFilesFolders()
 
+        self.url, self.key, self.delay, self.xmlHistory = self.getConfigParams()
+        self.httpClassification = self.checkApiKey()
 
-def createDir(*args):
-    log = getMyLogger()
-    for d in args:
-        if not os.path.exists(d):
-            log.debug('Creating Dir: {}'.format(d))
-            os.makedirs(d)
+    def checkApiKey(self):
+        # Nicht default Value und nicht leer
+        http = False
+        if not PARAM_DEF_KEY_VAL == self.key or not self.key:
+            self.logger.info('API-Key found. Enabling HTTP Requests.')
+            http = True
+        else:
+            self.logger.error('No API-Key defined in requester.ini')
+            self.logger.info('No HTTP Requests possible. Please specify API-Key.')
 
+        return http
 
-def createConfigTemplate():
-    getMyLogger().debug('Creating Config File: {}'.format(PARAM_REQ))
-    config = cfgp.ConfigParser()
-    config[PARAM_REQ] = {PARAM_URL: 'http://www.example.com',
-                         PARAM_KEY: PARAM_DEF_KEY_VAL,
-                         PARAM_DELAY: PARAM_DEF_DELAY_VAL}
-    return config
+    def checkFilesFolders(self):
+        self.logger.debug('Starting Folder check...')
+        self.createDir(dirIn, dirTrash, dirDone, dirError, dirOut)
 
+        if not os.path.exists(fileConfig):
+            self.logger.debug('Creating File: {}'.format(fileConfig))
+            with open(fileConfig, 'w') as cfgFile:
+                config = self.createConfigTemplate()
+                config.write(cfgFile)
 
-def getConfig():
-    config = cfgp.ConfigParser()
-    with open(fileConfig, 'r') as cfgFile:
-        config.read_file(cfgFile)
+        if not os.path.exists(fileHistory) and self.xmlHistory:
+            self.logger.debug('Creating File: {}'.format(fileHistory))
+            root = self.createHistoryTemplate()
+            self.xmlToFile(root)
+
+        self.logger.debug('Folder check done.')
+
+    def createDir(self, *args):
+        for d in args:
+            if not os.path.exists(d):
+                self.logger.debug('Creating Dir: {}'.format(d))
+                os.makedirs(d)
+
+    def createConfigTemplate(self):
+        self.logger.debug('Creating Config File: {}'.format(PARAM_REQ))
+        config = cfgp.ConfigParser()
+        config[PARAM_REQ] = {PARAM_URL: 'http://www.example.com',
+                             PARAM_KEY: PARAM_DEF_KEY_VAL,
+                             PARAM_DELAY: PARAM_DEF_DELAY_VAL,
+                             PARAM_XML: PARAM_DEF_XML_VAL}
         return config
 
+    def getConfig(self):
+        config = cfgp.ConfigParser()
+        with open(fileConfig, 'r') as cfgFile:
+            config.read_file(cfgFile)
+            return config
 
-def getConfigParams():
-    config = getConfig()
+    def getConfigParams(self):
+        config = self.getConfig()
 
-    url = None
-    key = None
-    delay = None
+        url = None
+        key = None
+        delay = None
+        xmlHistory = None
 
-    if config.has_option(PARAM_REQ, PARAM_URL):
-        url = config.get(PARAM_REQ, PARAM_URL)
+        if config.has_option(PARAM_REQ, PARAM_URL):
+            url = config.get(PARAM_REQ, PARAM_URL)
 
-    if config.has_option(PARAM_REQ, PARAM_KEY):
-        key = config.get(PARAM_REQ, PARAM_KEY)
+        if config.has_option(PARAM_REQ, PARAM_KEY):
+            key = config.get(PARAM_REQ, PARAM_KEY)
 
-    if config.has_option(PARAM_REQ, PARAM_DELAY):
-        delay = int(config.get(PARAM_REQ, PARAM_DELAY))
+        if config.has_option(PARAM_REQ, PARAM_DELAY):
+            delay = config.getint(PARAM_REQ, PARAM_DELAY)
 
-    return url, key, delay
+        if config.has_option(PARAM_REQ, PARAM_XML):
+            xmlHistory = config.getboolean(PARAM_REQ, PARAM_XML)
 
+        return url, key, delay, xmlHistory
 
-def sendRequest(img):
-    myUrl = url
-    myData = {PARAM_KEY: key}
-    myFiles = {'image': img}
+    def sendRequest(self, *imgs):
+        results = []
+        
+        if self.httpClassification:
+            myUrl = self.url
+            myData = {PARAM_KEY: self.key}
 
-    return requests.post(myUrl, data=myData, files=myFiles)
+            for img in imgs:
+                myFiles = {'image': img}
+                res = requests.post(myUrl, data=myData, files=myFiles)
+                results.append(res)
 
+        return results
 
-def createHistoryTemplate():
-    log = getMyLogger()
-    log.debug('Creating History Template: {}'.format(XML_ROOT))
-    root = etree.Element(XML_ROOT)
+    def createHistoryTemplate(self):
+        log = getMyLogger()
+        log.debug('Creating History Template: {}'.format(XML_ROOT))
+        root = etree.Element(XML_ROOT)
 
-    # Elemente erzeugen
-    request = etree.SubElement(root, XML_REQUEST)
-    result = etree.SubElement(request, XML_RESULT)
-    image = etree.SubElement(request, XML_IMAGE)
+        # Elemente erzeugen
+        request = etree.SubElement(root, XML_REQUEST)
+        result = etree.SubElement(request, XML_RESULT)
+        image = etree.SubElement(request, XML_IMAGE)
 
-    # Beispiel Attribute
-    request.attrib[XML_ATR_DATE] = XML_ATR_DATE_FORMAT
-    request.attrib['info_text'] = 'Beispieleintrag'
-    image.attrib[XML_ATR_DTYPE] = 'np.int32'
-    result.text = 'numpy.array([x,y])'
-    return root
+        # Beispiel Attribute
+        request.attrib[XML_ATR_DATE] = XML_ATR_DATE_FORMAT
+        request.attrib['info_text'] = 'Beispieleintrag'
+        image.attrib[XML_ATR_DTYPE] = 'np.int32'
+        result.text = 'numpy.array([x,y])'
+        return root
 
+    def addToXmlHistory(self, classArr, imageArr, now):
+        root = self.readXmlHistory()
 
-def addToHistory(classArr, imageArr, now):
-    root = readHistory()
+        # Neuen Request erzeugen
+        request = etree.SubElement(root, XML_REQUEST)
+        result = etree.SubElement(request, XML_RESULT)
+        image = etree.SubElement(request, XML_IMAGE)
 
-    # Neuen Request erzeugen
-    request = etree.SubElement(root, XML_REQUEST)
-    result = etree.SubElement(request, XML_RESULT)
-    image = etree.SubElement(request, XML_IMAGE)
+        # Attribute und Texte hinzufügen
+        request.attrib[XML_ATR_DATE] = now.strftime(XML_ATR_DATE_FORMAT)
+        image.attrib[XML_ATR_DTYPE] = imageArr.dtype.name
+        result.text = json.dumps(classArr)
+        image.text = json.dumps(imageArr.tolist())
 
-    # Attribute und Texte hinzufügen
-    request.attrib[XML_ATR_DATE] = now.strftime(XML_ATR_DATE_FORMAT)
-    image.attrib[XML_ATR_DTYPE] = imageArr.dtype.name
-    result.text = json.dumps(classArr)
-    image.text = json.dumps(imageArr.tolist())
+        self.xmlToFile(root)
 
-    xmlToFile(root)
+    def xmlToFile(self, root):
+        xmlString = etree.tostring(root, encoding='UTF-8', pretty_print=True)
+        with open(fileHistory, 'w') as hisFile:
+            hisFile.write(xmlString.decode('UTF-8'))
 
+    def readXmlHistory(self):
+        root = None
+        with open(fileHistory, 'r') as hisFile:
+            root = etree.fromstring(hisFile.read())
+        return root
 
-def xmlToFile(root):
-    xmlString = etree.tostring(root, encoding='UTF-8', pretty_print=True)
-    with open(fileHistory, 'w') as hisFile:
-        hisFile.write(xmlString.decode('UTF-8'))
+    def sendDirRequests(self):
+        if self.httpClassification:
+            _, _, files = next(os.walk(dirIn))
 
+            self.logger.info('Files to process {}'.format(len(files)))
 
-def readHistory():
-    root = None
-    with open(fileHistory, 'r') as hisFile:
-        root = etree.fromstring(hisFile.read())
-    return root
+            for f in files:
+                # Dateiname/pfad erzeugen
+                imgPath = os.path.join(dirIn, f)
 
+                # Nur PNG Bilder einlesen
+                if 'png' in imgPath.lower():
+                    self.logger.info('Opening file {}'.format(imgPath))
+                    img = open(imgPath, 'rb')
 
-log = initLogging()
-log.debug('Starting Folder check...')
-checkFilesFolders()
-log.debug('Folder check done')
-# Config Einträge prüfen
-# Ohne Api-Key nichts machen
-run = 0
-url, key, delay = getConfigParams()
+                    response = self.sendRequest(img)[0]
+                    img.close()
 
-# Nicht default Value und nicht leer
-if not PARAM_DEF_KEY_VAL == key or not key:
-    run = 1
-    log.info('API-Key found')
-else:
-    log.error('No API-Key defined in requester.ini')
-while run:
+                    dest = ''
+                    now = datetime.now()
+                    # Aktuelle Zeit für den Dateinamen nutzen
+                    newName = '{}.png'.format(
+                        now.strftime('%Y-%m-%d_%H_%M_%S'))
 
-    # while True: -- Hier kommt später die Schleife hin
-    # Input Ordner auf Bilder überprüfen
-    _, _, files = next(os.walk(dirIn))
+                    if response.ok:
+                        self.logger.info('Image sucessfully transfered')
 
-    log.info('Files to process {}'.format(len(files)))
+                        # Response verarbeiten
+                        classArr = response.json()
+                        # Bild als numpy Array einlesen
+                        imageArr = imageio.imread(imgPath)
 
-    for f in files:
-        # Dateiname/pfad erzeugen
-        imgPath = os.path.join(dirIn, f)
+                        # Anfrage zur Historie hinzufügen
+                        if self.xmlHistory:
+                            self.addToXmlHistory(classArr, imageArr, now)
 
-        # Nur PNG Bilder einlesen
-        if 'png' in imgPath.lower():
-            log.info('Opening file {}'.format(imgPath))
-            img = open(imgPath, 'rb')
+                        # Dateinamen für done Ordner
+                        dest = os.path.join(dirDone, newName)
+                    else:
+                        # Dateinamen done/error Ordner
+                        self.logger.error('Request error: {}'.format(response.text))
+                        dest = os.path.join(dirError, newName)
 
-            myData = {'key': key}
-            myFiles = {'image': img}
+                    self.logger.info('Moving file to: {}'.format(dest))
+                    os.rename(imgPath, dest)
+                else:
+                    self.logger.info('File "{}" is not an PNG moving to trash'.format(f))
 
-            response = requests.post(url, data=myData, files=myFiles)
-            img.close()
+                    trash = os.path.join(dirTrash, f)
+                    os.rename(imgPath, trash)
 
-            dest = ''
-            now = datetime.now()
-            # Aktuelle Zeit für den Dateinamen nutzen
-            newName = '{}.png'.format(now.strftime('%Y-%m-%d_%H_%M_%S'))
+                # Eine Sekunde warten damit die Requests nicht zu schnell bearbeitet werden.
+                # Evtl. unnötig, da die verarbeitung bereits länger als eine Sekunde dauert
+                time.sleep(1)
 
-            if response.ok:
-                log.info('Image sucessfully transfered')
+            time.sleep(self.delay)
 
-                # Response verarbeiten
-                classArr = response.json()
-                # Bild als numpy Array einlesen
-                imageArr = imageio.imread(imgPath)
-
-                # Anfrage zur Historie hinzufügen
-                addToHistory(classArr, imageArr, now)
-
-                # Dateinamen für done Ordner
-                dest = os.path.join(dirDone, newName)
-            else:
-                # Dateinamen done/error Ordner
-                log.error('Request error: {}'.format(response.text))
-                dest = os.path.join(dirError, newName)
-
-            log.info('Moving file to: {}'.format(dest))
-            os.rename(imgPath, dest)
-        else:
-            log.info('File "{}" is not an PNG moving to trash'.format(f))
-
-            trash = os.path.join(dirTrash, f)
-            os.rename(imgPath, trash)
-
-        # Eine Sekunde warten damit die Requests nicht zu schnell bearbeitet werden.
-        # Evtl. unnötig, da die verarbeitung bereits länger als eine Sekunde dauert
-        time.sleep(1)
-
-    time.sleep(delay)
+if __name__ == '__main__':
+    Requester()
